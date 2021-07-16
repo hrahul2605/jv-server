646fd629ab844fd129f63fc9a72d7f2d9fda171e
Rahul Halder <hrahul2605@gmail.com>
2021-07-16 12:45:08 +0530

    upd: many to many relation update for users vote to rivals

--- src/polls/polls.module.ts
+++ src/polls/polls.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/user/user.module';
 import { PollsController } from './polls.controller';
 import { PollsRepository } from './repositories/polls.repository';
 import { RivalsRepository } from './repositories/rivals.repository';
 import { PollsService } from './strategies/polls.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PollsRepository, RivalsRepository])],
+  imports: [
+    TypeOrmModule.forFeature([PollsRepository, RivalsRepository]),
+    UserModule,
+  ],
   controllers: [PollsController],
   providers: [PollsService],
   exports: [TypeOrmModule],
